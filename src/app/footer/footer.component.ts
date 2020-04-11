630660eb802bdd69b43aa2633fcd6614e8d01bff
AndreyKourov <andreykourov@mail.ru>
2020-04-12 00:11:56 +0600

    Исправил Задане1

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
+
+  footer = 'FOOTER в ANGULAR';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
